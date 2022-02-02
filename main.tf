b76b1a5477f58580b6723eec5da00ef6ebe274b8
michieldewilde <michieldw@spacelift.io>
2022-02-02 14:13:37 +0100

    Test

--- main.tf
+++ main.tf
@@ -20,6 +20,5 @@ resource "spacelift_stack" "example-stack" {
 resource "spacelift_webhook" "simple-webhook-4" {
   stack_id = spacelift_stack.example-stack.id
 
-  endpoint = "https://example.com/example_webhook"
-  secret   = "my_secret"
+  secret = "my_secret"
 }
